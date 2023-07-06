5474cf8da798449ba26e74b24f7c3304e806c48f
Ingo Albrecht <commit@promovicz.org>
2023-07-06 03:27:03 +0200

    Extend build, add rdrprep

--- build.sh
+++ build.sh
@@ -2,17 +2,36 @@
 
 set -e
 
+# Build rdrprep
+pushd rdrprep
+  make clean
+  make all
+  make install
+popd
+
+# Build/install JCC
 pushd jcc
   make clean
   make all
 popd
 
+# Build MVSGCC cross compilers
 pushd mvsgcc
   make clean
   make all-cross
+  #make install-cross
 popd
 
+# Build MVSCLIB runtime library
+pushd mvsclib
+  #make clean
+  #make all-cross
+  #make install-cross
+popd
+
+# Build MVSGCC target compilers
 pushd mvsgcc
   make all-target
+  #make install-target
 popd
 
