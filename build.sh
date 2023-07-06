e9772c8ad6a2210d3d9afc92de67bbb61561112b
Ingo Albrecht <commit@promovicz.org>
2023-07-06 07:37:56 +0200

    Add some install commands

--- build.sh
+++ build.sh
@@ -9,17 +9,18 @@ pushd rdrprep
   make install
 popd
 
-# Build/install JCC
+# Build JCC
 pushd jcc
   make clean
   make all
+  make install
 popd
 
 # Build MVSGCC cross compilers
 pushd mvsgcc
   make clean
   make all-cross
-  #make install-cross
+  make install-cross
 popd
 
 # Build MVSCLIB runtime library
@@ -32,6 +33,6 @@ popd
 # Build MVSGCC target compilers
 pushd mvsgcc
   make all-target
-  #make install-target
+  make install-target
 popd
 
